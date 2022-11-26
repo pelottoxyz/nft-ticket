c6de2c04664c89b7add287fd73c3e430cc8a355c
0xryk <ryk@nativo.studio>
2022-11-26 20:44:53 -0300

    update nft pass contract

--- nft/contracts/PelottoPass.sol
+++ nft/contracts/PelottoPass.sol
@@ -6,61 +6,54 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
-import "hardhat/console.sol";
 
 contract PelottoPass is ERC721, ERC721URIStorage, Pausable, Ownable {
   using Strings for uint256;
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  mapping(uint256 => string) public teams;
+  string public uriPrefix = "";
+  string public uriSuffix = ".json";
 
-  constructor() ERC721("PelottoPass", "PELOTTO") {
-    teams[1] = "Brazil";
-    teams[2] = "Argentina";
-    teams[3] = "Uruguay";
+  uint256 public maxSupply = 10;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _uriPrefix
+  ) ERC721(_name, _symbol) {
+    setUriPrefix(_uriPrefix);
   }
 
-  function generatePass(uint256 tokenId) public pure returns (string memory) {
-    string[8] memory parts;
-    parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 500 500">';
-    parts[1] = "<style>.base { fill: white; font-family: sans-serif; font-size: 14px; }</style>";
-    parts[2] = '<rect width="100%" height="100%" fill="black" />';
-    parts[3] = '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">PELOTTO</text>';
-    parts[4] = '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">';
-    parts[5] = Strings.toString(tokenId);
-    parts[6] = "</text></svg>";
-
-    bytes memory svg = abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6]);
-
-    return string(abi.encodePacked("data:image/svg+xml;base64,", Base64.encode(svg)));
+  function getTokenURI(uint256 tokenId) public view returns (string memory) {
+    string memory baseUri = _baseURI();
+    return string(abi.encodePacked(baseUri, tokenId.toString(), uriSuffix));
   }
 
-  function getTokenURI(uint256 tokenId, uint256 teamId) public view returns (string memory) {
-    bytes memory dataURI = abi.encodePacked(
-      "{",
-      '"name": "Pelotto Pass #',
-      tokenId.toString(),
-      '",',
-      '"description": "Pelotto Pass for ',
-      teams[teamId],
-      '",',
-      '"image": "',
-      generatePass(tokenId),
-      '"',
-      "}"
-    );
-
-    return string(abi.encodePacked("data:application/json;base64,", Base64.encode(dataURI)));
+  function totalSupply() public view returns (uint256) {
+    return _tokenIds.current();
   }
 
-  function mint(uint256 teamId) public whenNotPaused {
+  function mint() public payable whenNotPaused {
+    require(_tokenIds.current() <= maxSupply, "No more pass left to mint!");
     _tokenIds.increment();
     uint256 newItemId = _tokenIds.current();
     _safeMint(msg.sender, newItemId);
-    _setTokenURI(newItemId, getTokenURI(newItemId, teamId));
+    _setTokenURI(newItemId, getTokenURI(newItemId));
+  }
+
+  // utils
+
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
+  }
+
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
+  }
+
+  function setMaxSupply(uint256 _maxSupply) public onlyOwner {
+    maxSupply = _maxSupply;
   }
 
   function pause() public onlyOwner {
@@ -71,17 +64,24 @@ contract PelottoPass is ERC721, ERC721URIStorage, Pausable, Ownable {
     _unpause();
   }
 
-  function totalSupply() public view returns (uint256) {
-    return _tokenIds.current();
+  function withdraw() public onlyOwner {
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
   }
 
-  // The following functions are overrides required by Solidity.
+  // required
 
   function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
     super._burn(tokenId);
   }
 
-  function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+  function tokenURI(
+    uint256 tokenId
+  ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
     return super.tokenURI(tokenId);
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
   }
 }
--- nft/test/PelotoPass.ts
+++ nft/test/PelotoPass.ts
@@ -1,47 +1,49 @@
+import dotenv from 'dotenv'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import { PelottoPass, PelottoPass__factory } from '../typechain-types'
 
+dotenv.config()
+
 describe('PelottoPass contract', () => {
   let nftContractFactory: PelottoPass__factory
   let contract: PelottoPass
-  let _name = 'PelottoPass'
-  let _symbol = 'PELOTTO'
   let owner: SignerWithAddress
-  let account1: SignerWithAddress
+
+  const _name = 'PelottoPass'
+  const _symbol = 'PELOTTO'
+  const _uriPrefix = `ipfs://${process.env.CID}/`
 
   beforeEach(async () => {
     nftContractFactory = await ethers.getContractFactory(
       'PelottoPass'
     )
-    ;[owner, account1] = await ethers.getSigners()
+    ;[owner] = await ethers.getSigners()
     console.log('owner ', owner.address)
-    console.log('address ', account1.address)
   })
 
   describe('Deploy', () => {
     it('should deploy contract', async () => {
-      const nftContract = await nftContractFactory.deploy()
+      const nftContract = await nftContractFactory.deploy(
+        _name,
+        _symbol,
+        _uriPrefix
+      )
       contract = await nftContract.deployed()
       await contract.deployed()
       expect(await contract.name()).to.equal(_name)
       expect(await contract.symbol()).to.equal(_symbol)
     })
 
-    it('should mint a token', async () => {
-      await contract.mint(1)
+    it('should mint pass', async () => {
+      await contract.mint()
       expect(await contract.owner()).to.equal(owner.address)
     })
 
     it('totalSupply should be 2', async () => {
-      await contract.mint(1)
+      await contract.mint()
       expect(await contract.totalSupply()).to.equal(2)
-    })
-
-    it('should not continue without valid team id', async () => {
-      const minted = await contract.mint(2)
-      expect(minted).to.be.null
     })
   })
 })
