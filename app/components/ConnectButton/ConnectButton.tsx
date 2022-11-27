bec2a003996f83da66ee017ea2263d7e2454c29f
0xryk <ryk@nativo.studio>
2022-11-27 12:35:11 -0300

    add initial components to app

---
+++ app/components/ConnectButton/ConnectButton.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+
+import { styled } from '@/stitches'
+
+const Button = styled('button', {
+  appearance: 'none',
+  background: 'none',
+  border: 'none',
+  fontSize: 16,
+  fontFamily: '$system',
+  color: '$gray400',
+})
+
+export function ConnectWalletButton() {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== 'loading'
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus ||
+            authenticationStatus === 'authenticated')
+
+        return (
+          <div
+            {...(!ready && {
+              'aria-hidden': true,
+              style: {
+                opacity: 0,
+                pointerEvents: 'none',
+                userSelect: 'none',
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button
+                    onClick={openConnectModal}
+                    type="button"
+                    css={{
+                      borderRadius: 32,
+                      color: '$loContrast',
+                      background: '$accent',
+                      paddingInline: '$5',
+                      paddingBlock: '$3',
+                    }}
+                  >
+                    Connect Wallet
+                  </Button>
+                )
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <Button onClick={openChainModal} type="button">
+                    Wrong network
+                  </Button>
+                )
+              }
+
+              return (
+                <div style={{ display: 'flex', gap: 12 }}>
+                  <Button
+                    onClick={openChainModal}
+                    style={{ display: 'flex', alignItems: 'center' }}
+                    type="button"
+                  >
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          width: 16,
+                          height: 16,
+                          borderRadius: 999,
+                          overflow: 'hidden',
+                          marginRight: 6,
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <img
+                            alt={chain.name ?? 'Chain icon'}
+                            src={chain.iconUrl}
+                            style={{ width: 16, height: 16 }}
+                          />
+                        )}
+                      </div>
+                    )}
+                    {chain.name}
+                  </Button>
+
+                  <Button onClick={openAccountModal} type="button">
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ''}
+                  </Button>
+                </div>
+              )
+            })()}
+          </div>
+        )
+      }}
+    </ConnectButton.Custom>
+  )
+}
---
+++ app/components/index.tsx
@@ -0,0 +1,18 @@
+
+import { styled } from "@/stitches";
+
+export const Box = styled('div', {});
+
+export const Text = styled('p', {
+  color: '$gray300',
+  marginBottom: '$3',
+  fontSize: 16,
+  fontFamily: '$system'
+});
+
+export const Header = styled('div', {
+  padding: '$3',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'row'
+});
