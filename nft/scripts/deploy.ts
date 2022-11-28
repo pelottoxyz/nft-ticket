20d35aca47bc64577b529c276cf8ee3520fed7a9
0xryk <ryk@nativo.studio>
2022-11-27 23:49:29 -0300

    add logs

--- nft/scripts/deploy.ts
+++ nft/scripts/deploy.ts
@@ -4,15 +4,18 @@ import { ethers } from 'hardhat'
 dotenv.config()
 
 async function main() {
+  console.log('get contract factory...')
   const nftContractFactory = await ethers.getContractFactory(
     'PelottoPass'
   )
+  console.log('deploy contract...')
+  console.log(`CID: ${process.env.CID}`)
 
   const contract = await nftContractFactory.deploy(
     'PelottoPass',
     'PELOTTO',
     `ipfs://${process.env.CID}/`
-  )
+  )  
   await contract.deployed()
   console.log(`NFT contract deployed to ${contract.address}`)
 }
