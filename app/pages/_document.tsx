f5317e4c89192a5c9166248c5d16bc369ec371fe
0xryk <ryk@nativo.studio>
2022-11-28 12:34:35 -0300

    prettier

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -5,9 +5,16 @@ export default function MyDocument() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossorigin" />
-      <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="crossorigin"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
