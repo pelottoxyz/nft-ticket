15232bb23a96e6c4925b619cb0b1ebbdfa7310e1
0xryk <ryk@nativo.studio>
2022-11-28 00:59:13 -0300

    console log gas wei

--- nft/scripts/deploy.ts
+++ nft/scripts/deploy.ts
@@ -4,6 +4,11 @@ import { ethers } from 'hardhat'
 dotenv.config()
 
 async function main() {
+  console.log('getting gas price from oracle...')
+  const res = await fetch('https://gasstation-mumbai.matic.today/v2')
+  const json = await res.json()
+  const { standard: { maxPriorityFee } } = json
+  console.log('gas price', json)
   console.log('get contract factory...')
   const nftContractFactory = await ethers.getContractFactory(
     'PelottoNFTPass'
